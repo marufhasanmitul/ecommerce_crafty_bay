4647cda1f11b5b00b7b02bb871432219ddfda3d4
marufhasan <marufhasan136@gmail.com>
2023-09-14 00:26:09 +0600

    Categories Screen Completed

---
+++ lib/presentation/ui/screen/main_bottom_nav_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MainBottomNavScreen extends StatefulWidget {
+  const MainBottomNavScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MainBottomNavScreen> createState() => _MainBottomNavScreenState();
+}
+
+class _MainBottomNavScreenState extends State<MainBottomNavScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
